f7958e06b020b1c0fd6f4ad8fa63b1022dd116e8
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2024-02-10 15:30:56 -0300

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { Github } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <div className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">upload.ai</h1>
-        <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Developed with 💖 by Luca</span>
-          <Button size={"icon"} variant={"outline"}>
-            <Github />
-          </Button>
-        </div>
-      </div>
-    </div>
+    <main className="flex-1 p-6 flex gap-6 bg-red-200">
+      <div>content</div>
+      <div>sidebar</div>
+    </main>
   );
 }
